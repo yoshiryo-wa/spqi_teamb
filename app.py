8b87bebe73e384152876014f4f560885e9398718
NemotoLecomotive <nemoto.d@wingarc.com>
2023-10-11 11:16:03 +0900

    app.py 追加

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 app = Flask(__name__)
 
 # ルート（/）にアクセスがあったときの処理
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
  
@@ -17,11 +17,13 @@ def login():
   name = request.form['name']
   phone = request.form['phone']
   
-  payload = json.dumps({'name': name, 'phone': phone})
+  payload = ({'name': name, 'phone': phone})
+  headers = {'Content-Type': 'application/json'}
   
-  response = requests.post("http://at-qatest01.internaladds.wingarc.com:8080/api/v1/t_teamb/login", data=payload)
-  if response.status_code == 200:  # ステータスコードが200かどうかをチェック
-      return render_template('profile.html')
+  response = requests.post("http://at-qatest01.internaladds.wingarc.com:8080/api/v1/t_teamb/login", json=payload, headers=headers)
+  data = response.json()
+  if data.get("result"): # 'result'がtrueであれば
+      return render_template('profile.html', data=data)
   else:
       print(f"Error: {response.status_code}")  # エラーの場合はステータスコードを出力
 
